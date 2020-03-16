b696f6eb8beaf5c8354681608d323197ece7a753
Your Name In Single Quotes <your email here in single quotes>
2020-03-16 13:21:20 +0200

    done

---
+++ q2.js
@@ -0,0 +1,14 @@
+for (var i=0;i<5;i++){
+    // document.write(" ");     
+    // console.log(" ");
+    for(var j=0;j<=i;j++){
+
+        // console.log(" ");
+        // console.log("*");
+        // document.write(" ");
+        document.write("*");
+    
+    }
+    // document.write("*");
+    document.write("<br> ");  
+}
